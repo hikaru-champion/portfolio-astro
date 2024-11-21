8dc9246640d19e7ee6a1c7b5e38673e4a6c54794
hikaru-champion <hikaru.chanp0728@gmail.com>
2024-11-22 05:57:44 +0900

    add certification

---
+++ src/data/CloudCertifications.ts
@@ -0,0 +1,87 @@
+export type CloudCertificateItem = {
+  name: string,
+  image: string,
+  cloud: string,
+  activeDate: string
+}
+
+export const cloudCertificates: CloudCertificateItem[] = [
+  {
+    name: 'Google Cloud Certified - Cloud Digital Leader',
+    image: 'cloud-digital-leader.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/9'
+  },
+  {
+    name: 'Google Cloud Certified - Associate Cloud Engineer',
+    image: 'associate-cloud-engineer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2022/7'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud Architect',
+    image: 'professional-cloud-architect.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/10'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud Network Engineer',
+    image: 'professional-cloud-network-engineer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2023/8'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud Developer',
+    image: 'professional-cloud-developer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/5'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud DevOps Engineer',
+    image: 'professional-cloud-devops-engineer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/6'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud Security Engineer',
+    image: 'professional-cloud-security-engineer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/8'
+  },
+  {
+    name: 'Google Cloud Certified - Professional Cloud Database Engineer',
+    image: 'professional-cloud-database-engineer.png',
+    cloud: 'Google Cloud',
+    activeDate: '2024/9'
+  },
+  {
+    name: 'AWS Certified Cloud Practitioner',
+    image: 'aws-certified-cloud-practitioner.png',
+    cloud: 'AWS',
+    activeDate: '2021/1'
+  },
+  {
+    name: 'AWS Certified Solutions Architect – Associate',
+    image: 'aws-certified-solutions-architect-associate.png',
+    cloud: 'AWS',
+    activeDate: '2021/2'
+  },
+  {
+    name: 'AWS Certified Developer – Associate',
+    image: 'aws-certified-developer-associate.png',
+    cloud: 'AWS',
+    activeDate: '2021/8'
+  },
+  {
+    name: 'AWS Certified SysOps Administrator – Associate',
+    image: 'aws-certified-sysops-administrator-associate.png',
+    cloud: 'AWS',
+    activeDate: '2022/2'
+  },
+  {
+    name: 'Microsoft Certified: Azure Fundamentals',
+    image: 'microsoft-certified-azure-fundamentals.png',
+    cloud: 'Azure',
+    activeDate: '2021/9'
+  },
+]
