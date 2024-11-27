2bc72e7bc7a4115c8f0b2aaacb307073340683ce
hikaru-champion <hikaru.chanp0728@gmail.com>
2024-11-28 05:41:00 +0900

    refactor

---
+++ src/data/BiographyLists.ts
@@ -0,0 +1,19 @@
+export type biographyItem = {
+  date: string,
+  content: string
+}
+
+export const biographyList:biographyItem[] = [
+  {
+    date: '2019/3',
+    content: '大学院 電気電子情報工学専攻 卒業'
+  },
+  {
+    date: '2019/4',
+    content: 'ITコンサルティング企業に入社。アパレル業界のお客様を中心にAWS、Google Cloudを用いたクラウドインフラの設計～運用・保守までを担当'
+  },
+  {
+    date: '2023/9',
+    content: '事業会社に入社。Google CloudのCCoEとしてコスト最適化・ガイドラインの作成、技術支援を担当'
+  },
+]
---
+++ src/data/ProfileLists.ts
@@ -0,0 +1,27 @@
+export type profileItem = {
+  category: string,
+  item: string
+}
+
+export const profileList:profileItem[] = [
+  {
+    category: '名前',
+    item: 'Champion'
+  },
+  {
+    category: '年齢',
+    item: '29歳'
+  },
+  {
+    category: '住まい',
+    item: '埼玉県在住'
+  },
+  {
+    category: '趣味',
+    item: '筋トレ / テニス / 散歩 / ドライブ'
+  },
+  {
+    category: '好きなIT技術',
+    item: 'クラウド技術全般'
+  },
+]
