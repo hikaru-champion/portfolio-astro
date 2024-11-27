e7cb6b704118bf37e54fd9b43b8b1af3b6b1094e
hikaru-champion <hikaru.chanp0728@gmail.com>
2024-11-27 17:52:02 +0900

    fix

--- src/data/NavLists.ts
+++ src/data/NavLists.ts
@@ -2,4 +2,5 @@ export const navLinks: { label: string; href: string }[] = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about/" },
   { label: "Skills", href: "/skills/" },
+  { label: "Contact", href: "/contact/" },
 ]
