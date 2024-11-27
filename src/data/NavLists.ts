fc32a7e063c169c39ba426cd1c158a265205e264
hikaru-champion <hikaru.chanp0728@gmail.com>
2024-11-27 18:55:39 +0900

    delete contact form

--- src/data/NavLists.ts
+++ src/data/NavLists.ts
@@ -2,5 +2,4 @@ export const navLinks: { label: string; href: string }[] = [
   { label: "Home", href: "/" },
   { label: "About", href: "/about/" },
   { label: "Skills", href: "/skills/" },
-  { label: "Contact", href: "/contact/" },
 ]
